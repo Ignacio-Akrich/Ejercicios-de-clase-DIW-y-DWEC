234ca0ad80736c4f1046ad7ac205b30d7f9dffe4
Ignacio <jakrich@cifpfbmoll.eu>
2021-10-05 19:57:00 +0200

    Ejercicio 05/10/21

---
+++ src/DWEC/js/script.js
@@ -0,0 +1,7 @@
+function suma(){
+   let num1 = parseInt(document.getElementById("value1").value);
+   let num2 = parseInt(document.getElementById("value2").value);
+   let result =  num1 + num2;
+   document.getElementById("result").innerHTML = result;
+}
+
