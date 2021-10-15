52a069bff995c7ae4da672ebe2b6d0821a5a6ccf
Ignacio <jakrich@cifpfbmoll.eu>
2021-10-15 17:42:33 +0200

    Add

--- src/DWEC/js/script.js
+++ src/DWEC/js/script.js
@@ -1,7 +1,6 @@
-function suma(){
-   let num1 = parseInt(document.getElementById("value1").value);
-   let num2 = parseInt(document.getElementById("value2").value);
-   let result =  num1 + num2;
-   document.getElementById("result").innerHTML = result;
+function suma() {
+  let num1 = parseInt(document.getElementById("value1").value);
+  let num2 = parseInt(document.getElementById("value2").value);
+  let result = num1 + num2;
+  document.getElementById("result").innerHTML = result;
 }
-
